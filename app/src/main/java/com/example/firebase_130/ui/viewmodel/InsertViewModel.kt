f4970400ae3bee6a00d5e312ac70ac1fc3516669
Kevinzain29 <ahmad.kevin.ft22@mail.umy.ac.id>
2025-01-12 15:11:26 +0700

    add class InsertViewModel: fun validateFields, fun insertMhs on page InsertViewModel.kt

--- app/src/main/java/com/example/firebase_130/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/firebase_130/ui/viewmodel/InsertViewModel.kt
@@ -1,6 +1,64 @@
 package com.example.firebase_130.ui.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.firebase_130.model.Mahasiswa
+import com.example.firebase_130.repository.RepositoryMhs
+import kotlinx.coroutines.launch
+
+class InsertViewModel(
+    private val mhs: RepositoryMhs
+) : ViewModel() {
+    var uiEvent: InsertUiState by mutableStateOf(InsertUiState())
+        private set
+    var uiState: FormState by mutableStateOf(FormState.Idle)
+        private set
+    //memperbarui state berdasarkan input pengguna
+    fun updateState(mahasiswaEvent: MahasiswaEvent) {
+        uiEvent = uiEvent.copy(
+            insertUiEvent = mahasiswaEvent
+        )
+    }
+    //validasi data input pengguna
+    fun validateFields(): Boolean {
+        val event = uiEvent.insertUiEvent
+        val errorState = FormErrorState(
+            nim = if (event.nim.isNotEmpty()) null else "NIM tidak boleh kosong",
+            nama = if (event.nama.isNotEmpty()) null else "nama tidak boleh kosong",
+            alamat = if (event.alamat.isNotEmpty()) null else "alamat tidak boleh kosong",
+            gender = if (event.gender.isNotEmpty()) null else "gender tidak boleh kosong",
+            kelas = if (event.kelas.isNotEmpty()) null else "kelas tidak boleh kosong",
+            angkatan = if (event.angkatan.isNotEmpty()) null else "angkatan tidak boleh kosong"
+        )
+        uiEvent = uiEvent.copy(isEntryValid = errorState)
+        return errorState.isValid()
+    }
+    fun insertMhs() {
+        if (validateFields()) {
+            viewModelScope.launch {
+                uiState = FormState.Loading
+                try {
+                    mhs.insertMhs(uiEvent.insertUiEvent.toMhsModel())
+                    uiState = FormState.Success("Data berhasil disimpan")
+                } catch (e: Exception) {
+                    uiState = FormState.Error("Data gagal disimpan")
+                }
+            }
+        } else {
+            uiState = FormState.Error("Data tidak valid")
+        }
+    }
+    fun resetForm() {
+        uiEvent = InsertUiState()
+        uiState = FormState.Idle
+    }
+    fun resetSnackBarMessage() {
+        uiState = FormState.Idle
+    }
+}
 
 sealed class FormState {
     object Idle : FormState()
