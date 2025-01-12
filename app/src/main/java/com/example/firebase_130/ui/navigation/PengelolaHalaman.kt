ecb8197a880d0cc419f7e72eba7c035a52fb1c5a
Kevinzain29 <ahmad.kevin.ft22@mail.umy.ac.id>
2025-01-12 23:00:40 +0700

    add file PengelolaHalaman.kt: fun PengelolaHalaman

---
+++ app/src/main/java/com/example/firebase_130/ui/navigation/PengelolaHalaman.kt
@@ -0,0 +1,38 @@
+package com.example.firebase_130.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.firebase_130.ui.view.HomeScreen
+import com.example.firebase_130.ui.view.InsertMhsView
+
+@Composable
+fun PengelolaHalaman (
+    modifier: Modifier,
+    navController: NavHostController = rememberNavController()
+) {
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier
+    ) {
+        composable(DestinasiHome.route) {
+            HomeScreen(
+                navigateToitemEntry = {
+                    navController.navigate(DestinasiInsert.route)
+                },
+            )
+        }
+        composable(DestinasiInsert.route) {
+            InsertMhsView(
+                onBack = { navController.popBackStack() },
+                onNavigate = {
+                    navController.navigate(DestinasiHome.route)
+                }
+            )
+        }
+    }
+}
